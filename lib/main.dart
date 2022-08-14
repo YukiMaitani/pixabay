f85607d02f96ede8f1672ba19c37fd8db0507e02
yuki_maitani <fightzoi1997@gmail.com>
2022-08-14 22:28:54 +0900

    画像をAPIを使って検索し表示

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PixabayPage(),
+    );
+  }
+}
+
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({Key? key}) : super(key: key);
+
+  @override
+  State<PixabayPage> createState() => _PixabayPageState();
+}
+
+class _PixabayPageState extends State<PixabayPage> {
+  List imageList = [];
+
+  Future<void> fetchImages(String text) async {
+    Response response = await Dio().get(
+        'https://pixabay.com/api/?key=29261632-46d433d4c93a34434e822568f&q=$text&image_type=photo&per_page=100');
+    imageList = response.data['hits'];
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImages('花');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          initialValue: '花',
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (text) {
+            fetchImages(text);
+          },
+        ),
+      ),
+      body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: imageList.length,
+          itemBuilder: (context, index) {
+            Map<String, dynamic> image = imageList[index];
+            return InkWell(
+              onTap: (() {}),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    image['previewURL'],
+                    fit: BoxFit.cover,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                        color: Colors.white,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Icon(
+                              Icons.thumb_up_alt_outlined,
+                              size: 14,
+                            ),
+                            Text(image['likes'].toString()),
+                          ],
+                        )),
+                  )
+                ],
+              ),
+            );
+          }),
+    );
+  }
+}
